e9b958fc6b76970984f216bd80463106b4af18bf
Jesse Stone <jstone@psi.edu>
2021-06-14 15:43:41 -0700

    added all of the <1.15 schemas in the new location

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@
 
 #wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-1.16.0.0.shtml
 #wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-1.15.0.0.shtml
-#wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-pre-1.15.0.0.shtml
+wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-pre-1.15.0.0.shtml
