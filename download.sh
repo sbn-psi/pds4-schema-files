e5e8a04ceb30eac18ee0568ac2856a00b9c91380
Jesse Stone <jstone@psi.edu>
2022-07-25 08:53:41 -0700

    updated downloader script to use a list of URLs

--- download.sh
+++ download.sh
@@ -2,4 +2,5 @@
 
 #wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-1.16.0.0.shtml
 #wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-1.15.0.0.shtml
-wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-pre-1.15.0.0.shtml
+#wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-pre-1.15.0.0.shtml
+wget -r --no-host-directories --include-directories=/pds4 --timestamping --input-file=urls.txt
