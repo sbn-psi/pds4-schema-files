d87a5d819f2d13d4f018f84d048d5cf9eae9a1f6
Jesse Stone <jstone@psi.edu>
2022-07-27 13:33:47 -0700

    Hyb2 dictionary can be downloaded from PDS instead of JAXA

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@
 #wget -r --no-host-directories -I /pds4 -N https://pds.nasa.gov/datastandards/dictionaries/index-pre-1.15.0.0.shtml
 wget -r --no-host-directories --include-directories=/pds4 --timestamping --input-file=urls.txt
 
-mkdir -p pds4/mission/hyb2
-pushd pds4/mission/hyb2
-wget --no-host-directories --no-directories --no-parent --timestamping --input-file urls.txt
-popd
+#mkdir -p pds4/mission/hyb2
+#pushd pds4/mission/hyb2
+#wget --no-host-directories --no-directories --no-parent --timestamping --input-file urls.txt
+#popd
